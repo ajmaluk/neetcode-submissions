808f6c91622cd3cd8a27e00cc5dfdc92d044a588
MUHAMMED AJMAL U K <ajmaluk.me@gmail.com>
2026-07-25 11:13:40 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.length() != t.length()){
+            return false;
+         }
+         unordered_map<int,int> mp;
+         for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+         }
+         for(auto x : mp){
+            if(x.second != 0){
+                return false;
+            }
+         }
+         return true;
+    }
+};
