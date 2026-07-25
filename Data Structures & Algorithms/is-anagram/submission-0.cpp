bdd30ae6a31b62fa5495066fc476ac2fc5410cca
MUHAMMED AJMAL U K <ajmaluk.me@gmail.com>
2026-07-25 11:08:03 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.length() != t.length()){
+            return false;
+         }
+         unordered_map<int,int> mp;
+         for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+         }
+         for(int i=0;i<n;i++){
+            if(mp[s[i]]!=0){
+                return false;
+            }
+         }
+         return true;
+    }
+};
