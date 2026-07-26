3cdc2d258f73f7b8491c4dc5fbf316dfa2112ef7
MUHAMMED AJMAL U K <ajmaluk.me@gmail.com>
2026-07-26 11:52:36 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans="";
+        for(auto x : strs){
+            ans+=to_string(x.size())+"#"+x;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+
+        vector<string> ans;
+        int i = 0;
+
+        while (i < s.size()) {
+
+            int j = i;
+
+            while (s[j] != '#')
+                j++;
+
+            int len = stoi(s.substr(i, j - i));
+
+            ans.push_back(s.substr(j + 1, len));
+
+            i = j + 1 + len;
+        }
+
+        return ans;
+    }
+};
