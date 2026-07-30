aaf42bc643e3a6b6b7141d91e2a3da006464490e
MUHAMMED AJMAL U K <ajmaluk.me@gmail.com>
2026-07-30 14:37:04 +0530

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto x : tokens){
+            if (x != "+" && x != "-" && x != "*" && x != "/") {
+                st.push(stoi(x));
+            }else{
+                int num1 = st.top();
+                st.pop();
+                int num2 = st.top();
+                st.pop();
+                if(x=="+"){
+                   st.push(num2 + num1);
+                }else if(x=="-"){
+                   st.push(num2 - num1);
+                }else if(x=="*"){
+                   st.push(num2 * num1);
+                }else{
+                   st.push(num2 / num1);
+                }
+            }
+        }
+        
+        return st.top();
+    }
+};
